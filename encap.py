f22fe00f0128fff5acddf39f128c4857130b9180
Atharva Dilip Deshmukh <168166456+atharvadeshmukh18@users.noreply.github.com>
2026-01-09 23:38:35 +0530

    Add files via upload

---
+++ encap.py
@@ -0,0 +1,11 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+john = person("John", 30)
+john.greet()
+
+   
